539d13d9d70fa8da9b66f413448267308523a4af
Abit Gurung <gurungabit@gmail.com>
2020-07-19 04:52:33 -0500

    generated output to turn in

---
+++ run.sh
@@ -0,0 +1,5 @@
+python cacheSim.py -f A-9_new_1.5.pdf.trc -s 512 -b 4 -a 1 -r RR > Team_07_Sim_1_M#2.txt
+python cacheSim.py -f A-9_new_1.5.pdf.trc -s 512 -b 8 -a 2 -r RND > Team_07_Sim_2_M#2.txt
+python cacheSim.py -f A-9_new_1.5.pdf.trc -s 1024 -b 16 -a 16 -r RR > Team_07_Sim_3_M#2.txt
+python cacheSim.py -f A-9_new_1.5.pdf.trc -s 1024 -b 64 -a 8 -r RND > Team_07_Sim_4_M#2.txt
+python cacheSim.py -f A-9_new_1.5.pdf.trc -s 256 -b 4 -a 4 -r RND > Team_07_Sim_5_M#2.txt
